aff4267e347e68d256e3a0a8057b0e869c8efdcb
xherdan1234 <crisztianarriaga1492@gmail.com>
2024-07-10 14:14:03 -0600

    Initialize dependabot/npm_and_yarn/anime-wallpaper-3.1.0

---
+++ src/ririkoCli.js
@@ -0,0 +1,162 @@
+/**
+ * @author earnestangel https://github.com/RirikoAI/RirikoBot
+ */
+const readline = require("readline");
+const colors = require("colors");
+const NODE_ENV = process.env.NODE_ENV || "development";
+
+let initialized = false;
+let showPrompt = false; // Flag to control prompt display
+
+process
+  .on("unhandledRejection", (reason, p) => {
+    console.error(reason, "Unhandled Rejection at Promise", p);
+  })
+  .on("uncaughtException", (err) => {
+    console.error(err, "Uncaught Exception thrown");
+    console.oLog(err);
+  });
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false, // Disable default behavior of echoing input
+});
+
+const { overrideLoggers } = require("helpers/logger");
+overrideLoggers();
+
+// Override console.log
+console.log = function (...args) {
+  console.oLog(...args);
+  scrollToBottom();
+  rl.prompt();
+};
+
+rl.setPrompt("Ririko > ");
+
+const commands = new Map();
+
+// Register commands
+registerCommand({
+  name: "exit",
+  description: "Exit the terminal",
+  handler: exitCommand,
+});
+
+registerCommand({
+  name: "command0",
+  description: "Execute command0 without arguments",
+  handler: command0Handler,
+});
+
+registerCommand({
+  name: "command1",
+  description: "Execute command1 with 1 argument",
+  handler: command1Handler,
+});
+
+registerCommand({
+  name: "command2",
+  description: "Execute command2 with 2 arguments",
+  handler: command2Handler,
+});
+
+registerCommand({
+  name: "help",
+  description: "Display available commands",
+  handler: helpCommand,
+});
+
+function scrollToBottom() {
+  // Scroll output to the bottom
+  process.stdout.write("\x1B[1B\x1B[9999D");
+}
+
+function registerCommand(command) {
+  const { name, description, handler } = command;
+  commands.set(name, {
+    description,
+    handler,
+  });
+}
+
+function exitCommand() {
+  rl.close();
+}
+
+function command0Handler() {
+  console.log("Executing command0");
+  // Handle command with 0 arguments
+}
+
+function command1Handler(arg1) {
+  console.log(`Executing command1 with argument: ${arg1}`);
+  // Handle command with 1 argument
+}
+
+function command2Handler(arg1, arg2) {
+  console.log(`Executing command2 with arguments: ${arg1}, ${arg2}`);
+  // Handle command with 2 arguments
+}
+
+function helpCommand() {
+  let helpText = "";
+  commands.forEach((value, key) => {
+    helpText += `${key}: ${value.description}\n`;
+  });
+  console.log("Available commands:\n" + helpText);
+}
+
+rl.on("line", (input) => {
+  if (showPrompt) {
+    const args = input.trim().split(" ");
+    const command = args[0];
+
+    const commandHandler = commands.get(command);
+    if (commandHandler) {
+      const commandArgs = args.slice(1);
+      commandHandler.handler(...commandArgs);
+    } else {
+      console.log("Unknown command");
+    }
+
+    scrollToBottom();
+    rl.prompt();
+  }
+});
+
+rl.on("close", () => {
+  console.log("Custom terminal closed");
+  process.exit(0);
+});
+
+// Function to toggle prompt display
+function togglePrompt() {
+  showPrompt = !showPrompt;
+}
+
+// function to enable prompt display
+function enablePrompt() {
+  showPrompt = true;
+  rl.prompt();
+}
+
+function logWithCallback(message, callback) {
+  console.log(message);
+  process.nextTick(callback);
+}
+
+function logWithPromise(message) {
+  return new Promise((resolve) => {
+    console.log(message);
+    process.nextTick(resolve);
+  });
+}
+
+module.exports = {
+  rl: rl,
+  log: console.oLog,
+  togglePrompt: togglePrompt,
+  enablePrompt: enablePrompt,
+};
