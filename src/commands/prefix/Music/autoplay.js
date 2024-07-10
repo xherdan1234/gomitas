4cb6c7f8b3ca534b2191080832de6f6eb5656b35
xherdan1234 <crisztianarriaga1492@gmail.com>
2024-07-10 14:13:26 -0600

    Initialize snyk-fix-b615b5258f00a6afbabfa4ad9dc1ccd8

---
+++ src/commands/prefix/Music/autoplay.js
@@ -0,0 +1,23 @@
+module.exports = {
+  config: {
+    name: "autoplay",
+    aliases: ["p"],
+    description: "Toggle autoplay on or off.",
+    usage: "autoplay [on/off]",
+  },
+  category: "Music",
+  inVoiceChannel: true,
+  run: async (client, message) => {
+    const queue = client.player.getQueue(message);
+    if (!queue)
+      return message.channel.send(
+        `${client.config.emoji.error} | There is nothing in the queue right now!`
+      );
+    const autoplay = queue.toggleAutoplay();
+    message.channel.send(
+      `${client.config.emoji.success} | AutoPlay: \`${
+        autoplay ? "On" : "Off"
+      }\``
+    );
+  },
+};
---
+++ src/commands/prefix/Music/join.js
@@ -0,0 +1,28 @@
+const { Constants } = require("discord.js");
+
+module.exports = {
+  config: {
+    name: "join",
+    aliases: ["move"],
+    description: "Join the voice channel where the user is in",
+    usage: "join",
+  },
+  category: "Music",
+  run: async (client, message, args) => {
+    let voiceChannel = message.member.voice.channel;
+    if (args[0]) {
+      voiceChannel = await client.channels.fetch(args[0]);
+      if (!Constants.VoiceBasedChannelTypes.includes(voiceChannel?.type)) {
+        return message.channel.send(
+          `${client.config.emoji.error} | ${args[0]} is not a valid voice channel!`
+        );
+      }
+    }
+    if (!voiceChannel) {
+      return message.channel.send(
+        `${client.config.emoji.error} | You must be in a voice channel or enter a voice channel id!`
+      );
+    }
+    client.player.voices.join(voiceChannel);
+  },
+};
