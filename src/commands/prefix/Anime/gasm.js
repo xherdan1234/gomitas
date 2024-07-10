56cae779452f7087b2a4f34991f12f46ead790d1
xherdan1234 <crisztianarriaga1492@gmail.com>
2024-07-10 14:13:15 -0600

    Initialize giveaways

---
+++ src/commands/prefix/Anime/gasm.js
@@ -0,0 +1,41 @@
+const { MessageEmbed, EmbedBuilder } = require("discord.js");
+const fetch = require("node-fetch");
+const { getLang } = require("helpers/language");
+
+module.exports = {
+  config: {
+    name: "gasm",
+    description: "Generate random gasm images",
+    usage: "gasm",
+  },
+  category: "Anime",
+  owner: false,
+  /**
+   * Command runner
+   * @author earnestangel https://github.com/RirikoAI/RirikoBot
+   *
+   * @param {import("discord.js").Client} client Discord.js client
+   * @param {import("discord.js").Message | import("discord.js").CommandInteraction} message
+   * @param args Arguments, excludes the command name (e.g: !command args[0] args[1] args[2]...)
+   * @param prefix Guild specific prefix, falls back to config.js prefix
+   * @param {import("config")} config Config.js file
+   * @param {import("Quick.db").QuickDB} db Quick.db client
+   *
+   * @returns {Promise<*>}
+   */
+  run: async (client, message, args, prefix, config, db) => {
+    const lang = getLang();
+    await fetch("https://nekos.life/api/v2/img/gasm")
+      .then((res) => res.json())
+      .then((body) => {
+        const embed = new EmbedBuilder()
+          .setColor("Random")
+          .setDescription(`Requested by: ${message.author.toString()}`)
+          .setImage(body.url)
+          .setTimestamp()
+          .setFooter({ text: `${lang.footer1}` });
+
+        message.channel.send({ embeds: [embed] });
+      });
+  },
+};
